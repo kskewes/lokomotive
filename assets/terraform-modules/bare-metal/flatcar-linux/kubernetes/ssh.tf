8ab5a88dc20c7af56ee06a053b7d92ed7e11a8dc
Mateusz Gozdek <mateusz@kinvolk.io>
2020-11-05 17:27:32 +0100

    Print bootkube journal if it fails
    In previous attempt in e852117992a4a5ae13104a57ebb07dac71f9ed0f, I
    missed the fact that Terraform remote-exec only prints stderr to the
    user and stdout is ignored. This means, if we redirect journalctl output
    to stderr, then it should be printed which will improve debugging
    experience.
    
    Refs #440
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- assets/terraform-modules/bare-metal/flatcar-linux/kubernetes/ssh.tf
+++ assets/terraform-modules/bare-metal/flatcar-linux/kubernetes/ssh.tf
@@ -141,7 +141,7 @@ resource "null_resource" "bootkube-start" {
   provisioner "remote-exec" {
     inline = [
       "sudo mv $HOME/assets /opt/bootkube",
-      "sudo systemctl start bootkube",
+      "sudo systemctl start bootkube || (sudo journalctl -u bootkube >&2; exit 1)",
     ]
   }
 }
